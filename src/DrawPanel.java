5556c671bfa20d48d574f97cedbc19c06c3f561b
Zachary <zacharyhodge08@gmail.com>
2025-04-22 06:35:13 -0400

    Update DrawPanel.java
    Fix DrawPanel to visualize falling bricks with a mouse click and grid layout

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -2,15 +2,55 @@ import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
-import java.util.ArrayList;
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel implements MouseListener {
+
+    private BrickLayout layout;
 
     public DrawPanel() {
+        this.addMouseListener(this);
+        layout = new BrickLayout("src/bricks", 40, false);
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        int[][] grid = layout.getBrickLayout();
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                int x = 10 + c * 25;
+                int y = 10 + r * 25;
+                g.drawRect(x, y, 20, 20);
+                if (grid[r][c] == 1) {
+                    g2.setColor(Color.RED);
+                    g2.fillRect(x, y, 20, 20);
+                    g2.setColor(Color.BLACK);
+                }
+            }
+        }
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        layout.doOneBrick();
+        repaint();
+    }
+
+    @Override public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override public void mouseExited(MouseEvent e) {
+
+    }
 }
